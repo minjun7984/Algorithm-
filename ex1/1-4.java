0853552e0b0a285b909f061a033e029531d15f9f
Jung Min Jun <54847941+minjun7984@users.noreply.github.com>
2022-04-19 23:58:19 +0900

    문자뒤집기(수정)

--- ex1/1-4.java
+++ ex1/1-4.java
@@ -1,26 +1,28 @@
 import java.util.*;
   
+import java.util.*;
+
 class Main {
-  public ArrayList<String> solution(int n, String[] str){
-    ArrayList<String> answer = new ArrayList<>();
-    for(String x : str){
-      String tmp = new StringBuilder(x).reverse().toString();
-      answer.add(tmp);
+    public ArrayList<String> solution(int n, String[] str){
+        ArrayList<String> answer = new ArrayList<>();
+        for(String x : str){
+            String tmp = new StringBuilder(x).reverse().toString();
+            answer.add(tmp);
+        }
+
+        return answer;
     }
-        
-    return answer;
-  }
-  
-  public static void main(String[] args){
-	Main T = new Main();
-    Scanner kb = new Scanner(System.in);
-    int n = kb.nextInt();
-    String[] str = new String[n];
-    for(int i = 0; i<n; i++){
-      	str[i]=kb.next();
-  	}
-    for(String x : T.solution(n, str)){
-      	System.out.println(x);
-	  }
-   }
+
+    public static void main(String[] args){
+        Main T = new Main();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        String[] str = new String[n];
+        for(int i = 0; i<n; i++){
+            str[i]=kb.next();
+        }
+        for(String x : T.solution(n, str)){
+            System.out.println(x);
+        }
+    }
 }
