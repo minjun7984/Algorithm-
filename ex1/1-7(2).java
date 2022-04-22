f78c65351f81753f7aa7ef719e75293403263d7e
Jung Min Jun <54847941+minjun7984@users.noreply.github.com>
2022-04-22 22:27:56 +0900

    회문문자열 판단하기(

---
+++ ex1/1-7(2).java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+  
+class Main {
+  public String solution(String str){
+    String answer="NO";
+	  String tmp = new StringBuilder(str).reverse().toString();
+    if(str.equalsIgnoreCase(tmp))
+       answer = "YES";
+    return answer;
+  }
+  public static void main(String[] args){
+	Main T = new Main();
+    Scanner kb= new Scanner(System.in);
+    String str = kb.next();
+    System.out.print(T.solution(str));
+  }
+}
